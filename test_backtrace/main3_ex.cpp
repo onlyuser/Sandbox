8ee4102018c09924789f3d0d551f2ada1ebcb8f2
Jerry Chen <onlyuser@gmail.com>
2013-04-30 00:20:20 +0800

    waypoint ckin

--- test_backtrace/main3_ex.cpp
+++ test_backtrace/main3_ex.cpp
@@ -20,38 +20,29 @@
 #include <stdarg.h>
 #include <vector>
 
-char* exe = 0;
+//#define DEBUG
 
-static void initialiseExecutableName()
+static std::string argv0()
 {
-    char link[1024];
-    exe = new char[1024];
-    snprintf(link, sizeof(link), "/proc/%d/exe", getpid());
-    if(readlink(link, exe, sizeof(link)) == -1)
-    {
-        fprintf(stderr, "ERRORRRRR\n");
-        exit(1);
-    }
+    char buf[1024];
+    ssize_t len = readlink("/proc/self/exe", buf, sizeof(buf)-1);
+    if(len == -1)
+        return "";
+    buf[len] = '\0';
+    return buf;
 }
 
-static const char* getExecutableName()
-{
-    if(!exe)
-        initialiseExecutableName();
-    return exe;
-}
-
-static std::string _exec(std::string cmd)
+static std::string system_capture_stdout(std::string cmd)
 {
     FILE* pipe = popen(cmd.c_str(), "r");
     if(!pipe)
         return "ERROR";
-    char buffer[128];
+    char pipe_buf[128];
     std::string result = "";
     while(!feof(pipe))
     {
-        if(fgets(buffer, 128, pipe))
-            result += buffer;
+        if(fgets(pipe_buf, sizeof(pipe_buf), pipe))
+            result += pipe_buf;
     }
     pclose(pipe);
     return result;
@@ -89,6 +80,27 @@ static bool match_regex(std::string s, std::string pattern, int nmatch, ...)
     return result;
 }
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void launch_gdb_session()
+{
+    char pid_buf[30];
+    sprintf(pid_buf, "%d", getpid());
+    char name_buf[512];
+    name_buf[readlink("/proc/self/exe", name_buf, 511)] = 0;
+    int child_pid = fork();
+    if(!child_pid)
+    {
+        execlp("gdb", "-n", "-ex", "bt", name_buf, pid_buf, NULL);
+        abort(); // if gdb failed to start
+    }
+    else
+        waitpid(child_pid, NULL, 0);
+}
+
 // get REG_EIP from ucontext.h
 //#define __USE_GNU
 #include <ucontext.h>
@@ -99,20 +111,18 @@ static bool match_regex(std::string s, std::string pattern, int nmatch, ...)
 
 void bt_sighandler(int sig, siginfo_t* info, void* secret)
 {
-    std::cout << "stack array for " << getExecutableName() << " pid=" << getpid() << std::endl;
+    std::cout << "stack array for " << argv0() << " pid=" << getpid() << std::endl;
     std::cout << "Error: signal " << sig << ":" << std::endl;
     ucontext_t* uc = (ucontext_t*)secret;
     void* array[16];
     int size = backtrace(array, 16);
     array[1] = reinterpret_cast<void*>(uc->uc_mcontext.gregs[REG_EIP]);
     char** symbols = backtrace_symbols(array, size);
-    size_t length = 256;
-    char* output_buffer = (char*)malloc(length);
-    for(int i=1; i<size; ++i)
+    for(int i = 1; i<size; i++)
     {
         std::stringstream ss;
-        ss << "basename `addr2line " << array[i] << " -e " << getExecutableName() << "`";
-        std::string filename = _exec(ss.str());
+        ss << "basename `addr2line " << array[i] << " -e " << argv0() << "`";
+        std::string filename = system_capture_stdout(ss.str());
         filename = filename.substr(0, filename.length()-1);
         std::string module, mangled_name, offset, address;
         bool regex_status =
@@ -125,14 +135,14 @@ void bt_sighandler(int sig, siginfo_t* info, void* secret)
         if(regex_status)
         {
             int status;
-            char* ret = abi::__cxa_demangle(mangled_name.c_str(), output_buffer, &length, &status);
+            char* demangled_name = abi::__cxa_demangle(mangled_name.c_str(), NULL, 0, &status);
             if(status == 0)
             {
-                output_buffer = ret;
                 std::cout << "#" << i
                         << "  0x" << std::setfill('0') << std::setw(16) << std::hex
                         << reinterpret_cast<size_t>(array[i])
-                        << " in " << output_buffer << " at " << filename << std::endl;
+                        << " in " << demangled_name << " at " << filename << std::endl;
+                free(demangled_name);
             }
             else
             {
@@ -145,6 +155,10 @@ void bt_sighandler(int sig, siginfo_t* info, void* secret)
         else
             std::cout << "#" << i << "  " << symbols[i] << std::endl;
     }
+    free(symbols);
+#ifdef DEBUG
+    launch_gdb_session();
+#endif
     exit(0);
 }
 
