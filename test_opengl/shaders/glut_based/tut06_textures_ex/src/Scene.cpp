7621a558de9284c874203db36252b261b5669da1
Jerry Chen <onlyuser@gmail.com>
2014-05-15 01:35:40 +0800

    waypoint ckin

--- test_opengl/shaders/glut_based/tut06_textures_ex/src/Scene.cpp
+++ test_opengl/shaders/glut_based/tut06_textures_ex/src/Scene.cpp
@@ -64,25 +64,25 @@ void Scene::reset()
 
 void Scene::render()
 {
+    glm::vec3 camera_pos = m_camera->get_origin();
+    m_camera_pos[0] = camera_pos.x;
+    m_camera_pos[1] = camera_pos.y;
+    m_camera_pos[2] = camera_pos.z;
+    int i = 0;
+    for(lights_t::const_iterator q = m_lights.begin(); q != m_lights.end(); q++) {
+        glm::vec3 light_pos = (*q)->get_origin();
+        m_light_pos[i*3+0] = light_pos.x;
+        m_light_pos[i*3+1] = light_pos.y;
+        m_light_pos[i*3+2] = light_pos.z;
+        glm::vec3 light_color = (*q)->get_color();
+        m_light_color[i*3+0] = light_color.r;
+        m_light_color[i*3+1] = light_color.g;
+        m_light_color[i*3+2] = light_color.b;
+        i++;
+    }
     glClearColor(1, 1, 1, 1);
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     for(meshes_t::const_iterator p = m_meshes.begin(); p != m_meshes.end(); p++) {
-        glm::vec3 camera_pos = m_camera->get_origin();
-        m_camera_pos[0] = camera_pos.x;
-        m_camera_pos[1] = camera_pos.y;
-        m_camera_pos[2] = camera_pos.z;
-        int i = 0;
-        for(lights_t::const_iterator q = m_lights.begin(); q != m_lights.end(); q++) {
-            glm::vec3 light_pos = (*q)->get_origin();
-            m_light_pos[i*3+0] = light_pos.x;
-            m_light_pos[i*3+1] = light_pos.y;
-            m_light_pos[i*3+2] = light_pos.z;
-            glm::vec3 light_color = (*q)->get_color();
-            m_light_color[i*3+0] = light_color.r;
-            m_light_color[i*3+1] = light_color.g;
-            m_light_color[i*3+2] = light_color.b;
-            i++;
-        }
         (*p)->get_brush()->set_xform(m_camera->get_xform()*(*p)->get_xform());
         (*p)->get_brush()->set_local_xform((*p)->get_local_xform());
         (*p)->get_brush()->set_camera_pos(m_camera_pos);
