0ab0d33e89809122e7ab414e18e73e0d63f68a0f
Jerry Chen <onlyuser@gmail.com>
2014-05-25 15:21:35 +0800

    waypoint ckin

--- test_opengl/shaders/glut_based/tut06_textures_ex/src/skybox.f.glsl
+++ test_opengl/shaders/glut_based/tut06_textures_ex/src/skybox.f.glsl
@@ -1,11 +1,3 @@
-//varying vec2        f_texcoord;
-//uniform samplerCube mytexture;
-//
-//void main(void) {
-//    vec2 flipped_texcoord = vec2(f_texcoord.x, 1-f_texcoord.y);
-//    gl_FragColor = textureCube(mytexture, vec3(flipped_texcoord, 1));
-//}
-
 uniform samplerCube mytexture;
 
 void main()
--- test_opengl/shaders/glut_based/tut06_textures_ex/src/skybox.v.glsl
+++ test_opengl/shaders/glut_based/tut06_textures_ex/src/skybox.v.glsl
@@ -1,13 +1,3 @@
-//attribute vec3 coord3d;
-//attribute vec2 texcoord;
-//uniform   mat4 mvp_xform;
-//varying   vec2 f_texcoord;
-//
-//void main(void) {
-//    gl_Position = mvp_xform*vec4(coord3d, 1);
-//    f_texcoord = texcoord;
-//}
-
 uniform mat4 inv_normal_xform;
 uniform mat4 inv_projection_xform;
 
