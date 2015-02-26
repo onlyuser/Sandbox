3926643e6bc0d773d31891022b4cedbb10980bfa
Jerry Chen <onlyuser@gmail.com>
2015-02-27 02:36:26 +0800

    add comments

--- test_c/pre_0x/main3.cpp
+++ test_c/pre_0x/main3.cpp
@@ -16,15 +16,21 @@ int main(int argc, char** argv)
 {
     int x = 0;
 
+    // function declaration!! -- because "int(x)" is interpreted as "int x"
+    // NOTE: "int(x)" is not a typecast
     int y(int(x));
     std::cout << typeid(y).name() << std::endl; // int (int)
 
+    // variable initialization -- because "g" is not a type
     int f(g(x));
     std::cout << typeid(f).name() << std::endl; // int
 
+    // function declaration!! -- because "h" is a type
     int f2(h(x));
     std::cout << typeid(f2).name() << std::endl; // int (int (*)(int))
 
+    // function declaration!! -- because "qwe" is a type
+    // NOTE: "qwe()" is not a default constructor call
     qwe f3(qwe());
     std::cout << typeid(f3).name() << std::endl; // qwe (qwe (*)())
 
