17b0d9c0b06c3b140c0bf9a29cecaa300e6bf024
Jerry Chen <onlyuser@gmail.com>
2015-08-08 18:24:33 +0800

    waypoint ckin

--- test_ncurses/main.cpp
+++ test_ncurses/main.cpp
@@ -1,12 +1,11 @@
 #include <ncurses.h>
 
-int main()
+int main(int argc, char** argv)
 {
-    initscr();          /* Start curses mode          */
-    printw("Hello World !!!");  /* Print Hello World          */
-    refresh();          /* Print it on to the real screen */
-    getch();            /* Wait for user input */
-    endwin();           /* End curses mode        */
-
+    initscr();                 // Start curses mode
+    printw("Hello World !!!"); // Print Hello World
+    refresh();                 // Print it on to the real screen
+    getch();                   // Wait for user input
+    endwin();                  // End curses mode
     return 0;
 }
