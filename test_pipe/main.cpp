3b418b6f7e3a12e412ea694286bf9b3b37e6b372
Jerry Chen <onlyuser@gmail.com>
2016-06-04 10:31:03 +0800

    waypoint ckin

--- test_pipe/main.cpp
+++ test_pipe/main.cpp
@@ -1,9 +1,9 @@
 // REFERENCE:
-// "How to get file descriptor of buffer in memory?"
-// http://stackoverflow.com/questions/1558772/how-to-get-file-descriptor-of-buffer-in-memory
+// "How to get file descriptor of bufer in memory?"
+// http://stackoverflow.com/questions/1558772/how-to-get-file-descriptor-of-bufer-in-memory
 
 // CONCEPTS:
-// * convert in-memory buffer to FILE*
+// * convert in-memory bufer to FILE*
 // * convert file descriptor to FILE*
 // * pipe std-out from one process to std-in of another process
 
@@ -20,14 +20,15 @@ int main()
     if(!child_pid) { // parent process
         close(p[0]); // close read-channel -- indicates we're writing
         FILE* file = fdopen(p[1], "w");
-        char write_buff[] = "qwe\nasd\n";
-        fwrite(write_buff, sizeof(char), strlen(write_buff)+1, file);
+        char buf[] = "qwe\nasd\n";
+        fwrite(buf, sizeof(char), strlen(buf)+1, file);
     } else {         // child process
         close(p[1]); // close write-channel -- indicates we're reading
         FILE* file = fdopen(p[0], "r");
-        char read_buff[100];
-        fread(read_buff, sizeof(read_buff), 1, file);
-        std::cout << "result: \"" << read_buff << "\"" << std::endl;
+        size_t n = 100;
+        char buf[n];
+        fread(buf, sizeof(char), n, file);
+        std::cout << "result: \"" << buf << "\"" << std::endl;
     }
     return 0;
 }
